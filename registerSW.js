9207fbd78c6021e4c76dc00c9d25a218921ae6bd
bodik1987 <bsshul@gmail.com>
2024-12-04 09:26:09 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/budget_24/sw.js', { scope: '/budget_24/' })})}
